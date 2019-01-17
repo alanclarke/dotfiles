3697f660e647d8739234ecd07e82ca33a25833db
Alan Clarke <hi@alz.so>
2019-01-17 13:54:10 +0000

    Update atom.sh

--- scripts/config/atom.sh
+++ scripts/config/atom.sh
@@ -2,10 +2,8 @@
 
 # atom
 apm install atom-beautify
-apm install color-picker
-apm install format-standard
 apm install linter
 apm install linter-js-standard
-apm install merge-conflicts
 apm install open-on-github
 apm install sort-lines
+apm install prettier-standard-formatter
